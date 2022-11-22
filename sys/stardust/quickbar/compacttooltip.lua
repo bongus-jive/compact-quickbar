bc724aa1a80a8b57190e0ed7d43b371327b3995c
patman <27874300+patmanf@users.noreply.github.com>
2022-11-22 16:31:00 -0600

    createTooltip can just return a string lmao

--- sys/stardust/quickbar/compacttooltip.lua
+++ sys/stardust/quickbar/compacttooltip.lua
@@ -1,22 +1,9 @@
+local oldtooltip = createTooltip
 function createTooltip(screenPosition)
   for widgetName, label in pairs(compactLabels) do
     if widget.inMember(widgetName, screenPosition) then
-      local tooltip = config.getParameter("tooltipLayout")
-      
-      tooltip.text.value = label
-      if not tooltip.text.value or #tooltip.text.value == 0 then return end
-      
-      -- , yeah
-      local text = string.gsub(tooltip.text.value, "(^.-;)", "") --remove colors
-      local _,smalls = string.gsub(text, "[il.,:;'| ]", "") --small characters . i guess
-      local _,m = string.gsub(text, "[MmWw]", "") --m
-      local size = math.max(tooltip.background.size[1], math.ceil((#text + (m * 1.4) - (smalls * 0.6)) * 7.1))
-      
-      tooltip.background.size[1] = size
-      tooltip.text.position[1] = size / 2
-      tooltip.bg.fileBody = tooltip.bg.fileBody.."?scale="..size..";1"
-      
-      return tooltip
+      return label
     end
   end
+  if oldtooltip then oldtooltip(screenPosition) end
 end
